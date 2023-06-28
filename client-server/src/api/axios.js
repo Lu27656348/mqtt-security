a5d725cf258a3b8b75f89ebcf3dbf536e3c8ee21
Oliver Torres <olivertorres1997@gmail.com>
2023-06-28 16:17:17 -0400

    beleta

--- client-server/src/api/axios.js
+++ client-server/src/api/axios.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+import axios from 'axios';
 
 export default axios.create({
     baseURL: process.env.APLICATION_SERVER
--- client-server/src/server.js
+++ client-server/src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mqtt = require('mqtt');
-import axios from "./api/axios";
+// import axios from "./api/axios";
 
 app.use(express.json());
 
@@ -17,101 +17,106 @@ client.on('connect', () => {
             password:'Hola',
           };
 
-  axios.post(`auth/authorizacion`,data)
-  .then(response => {
-      if(response.data.token){
-        this.token=response.data.token;
-      }
-      const headers = {
-        'Authorization': `Bearer ${token}`,
-        'Accept': 'application/json',
-        'Custom-Header': 'Custom-Value'
-      };
-      //obtener todos los topicos y suscribirse
-      axios.get(`auth/all-topic`, {
-        headers: headers
-      })
-      .then(response => {
-          // const topics = response.data; 
-          // topics.forEach(topic => {
-          //   client.subscribe(topic, (err) => {
-          //     if (err) {
-          //       console.error('Error al suscribirse al topic:', topic);
-          //     }
-          //   });
-          // }); 
-          console.log('hola');
-          res.status(200).send('OK');
-        })
-        .catch(error => {
-          console.error('Error al obtener la lista de tópicos:', error);
-          res.status(500).send('Error al obtener la lista de tópicos');
-        });
-  })
-  .catch(error => {
-    console.error('No se pudo hacer login:', error);
-  });
+          fetch('auth/authorizacion', {
+            method: 'POST',
+            body: JSON.stringify(data)
+          })
+          .then(response => response.json())
+          .then(data => {
+            if (data.token) {
+              this.token = data.token;
+            }
+            const headers = {
+              'Authorization': `Bearer ${token}`,
+              'Accept': 'application/json',
+              'Custom-Header': 'Custom-Value'
+            };
+            fetch('auth/all-topic', {
+              method: 'GET',
+              headers: headers
+            })
+            .then(response => response.json())
+            .then(data => {
+              const topics = data; 
+              topics.forEach(topic => {
+                client.subscribe(topic, (err) => {
+                  if (err) {
+                    console.error('Error al suscribirse al topic:', topic);
+                  }
+                });
+              }); 
+              console.log('hola');
+              res.status(200).send('OK');
+            })
+            .catch(error => {
+              console.error('Error al obtener la lista de tópicos:', error);
+              res.status(500).send('Error al obtener la lista de tópicos');
+            });
+          })
+          .catch(error => {
+            console.error('No se pudo hacer login:', error);
+          });
 });
 
-//Suscribirse a un topico (Nuevo Lector)
-app.post('/mqtt/subscribe', (req, res) => {
-  const { topic } = req.body;
-  if (!topic) {
-    return res.status(400).send('Debe proporcionar un topic');
-  }
+// //Suscribirse a un topico (Nuevo Lector)
+// app.post('/mqtt/subscribe', (req, res) => {
+//   const { topic } = req.body;
+//   if (!topic) {
+//     return res.status(400).send('Debe proporcionar un topic');
+//   }
   
-  client.subscribe(topic, (err) => {
-    if (err) {
-      console.error('Error al suscribirse al topic:', topic);
-      res.status(500).send('Error al suscribirse al topic');
-    } else {
-      console.log('Suscrito al topic:', topic);
-      res.status(200).send('Suscrito al topic: ' + topic);
-    }
-  });
-});
+//   client.subscribe(topic, (err) => {
+//     if (err) {
+//       console.error('Error al suscribirse al topic:', topic);
+//       res.status(500).send('Error al suscribirse al topic');
+//     } else {
+//       console.log('Suscrito al topic:', topic);
+//       res.status(200).send('Suscrito al topic: ' + topic);
+//     }
+//   });
+// });
 
-//Des-suscribirse de un topico (eliminar lector)
-app.post('/mqtt/unsubscribe', (req, res) => {
-  const { topic } = req.body;
-  if (!topic) {
-    return res.status(400).send('Debe proporcionar un topic');
-  }
+// //Des-suscribirse de un topico (eliminar lector)
+// app.post('/mqtt/unsubscribe', (req, res) => {
+//   const { topic } = req.body;
+//   if (!topic) {
+//     return res.status(400).send('Debe proporcionar un topic');
+//   }
   
-  client.unsubscribe(topic, (err) => {
-    if (err) {
-      console.error('Error al desuscribirse del topic:', topic);
-      res.status(500).send('Error al desuscribirse del topic');
-    } else {
-      console.log('Desuscripto del topic:', topic);
-      res.status(200).send('Desuscripto del topic: ' + topic);
-    }
-  });
-});
+//   client.unsubscribe(topic, (err) => {
+//     if (err) {
+//       console.error('Error al desuscribirse del topic:', topic);
+//       res.status(500).send('Error al desuscribirse del topic');
+//     } else {
+//       console.log('Desuscripto del topic:', topic);
+//       res.status(200).send('Desuscripto del topic: ' + topic);
+//     }
+//   });
+// });
 
-//Recibe los mensajes que vienen del broker
-client.on('message', function (topic, message) {
-  console.log('Mensaje recibido en el topic:', topic, 'mensaje: ', message.toString());
+// //Recibe los mensajes que vienen del broker
+// client.on('message', function (topic, message) {
+//   console.log('Mensaje recibido en el topic:', topic, 'mensaje: ', message.toString());
 
-  // Parsear el mensaje como JSON
-  const data = JSON.parse(message.toString());
+//   // Parsear el mensaje como JSON
+//   const data = JSON.parse(message.toString());
 
-  // Enviar una petición POST al Aplication Server para validar permisos y retornar respuesta al broker
-  axios.post('auth/validation-permission', data)
-  .then(response => {    
-    // Comprobar si el campo "status" de la respuesta es "success"
-    if (response.data.status === 'success') {
-      // Si el campo "status" es "success", publicar la respuesta en el broker MQTT
-      client.publish(`${topic}/escucha`, JSON.stringify(response.data));
-    } else {
-      // Si el campo "status" no es "success", no publicar la respuesta
-      console.log('El mensaje recibido no es proveniente de un lector valido');
-    }
-  })
-  .catch(error => {
-    console.error('Error al enviar la petición POST:', error);
-  });
-});
+//   // Enviar una petición POST al Aplication Server para validar permisos y retornar respuesta al broker
+//   axios.post('auth/validation-permission', data)
+//   .then(response => {    
+//     // Comprobar si el campo "status" de la respuesta es "success"
+//     if (response.data.status === 'success') {
+//       // Si el campo "status" es "success", publicar la respuesta en el broker MQTT
+//       client.publish(`${topic}/escucha`, JSON.stringify(response.data));
+//     } else {
+//       // Si el campo "status" no es "success", no publicar la respuesta
+//       console.log('El mensaje recibido no es proveniente de un lector valido');
+//     }
+//   })
+//   .catch(error => {
+//     console.error('Error al enviar la petición POST:', error);
+//   });
+// });
 
 app.listen(process.env.PORT || 3030, function () {
   console.log(`Server listening on port ${process.env.PORT || 3030}`);
