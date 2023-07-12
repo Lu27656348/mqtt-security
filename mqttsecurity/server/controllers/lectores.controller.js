f9f2801341fcb69b4145d032c1e6e1ec64f9c9f8
Jesus Alfonzo <jesusalfonzo97@gmail.com>
2023-07-12 12:01:05 -0400

    getlector

--- mqttsecurity/server/controllers/lectores.controller.js
+++ mqttsecurity/server/controllers/lectores.controller.js
@@ -1,11 +1,15 @@
 import {pool} from '../db.js';
 
 export const getLectores = async (req,res)=>{
+    console.log('Obteniendo lectores');
     const [result] = await pool.query('SELECT * FROM lectores ORDER BY createdAt ASC');
     res.json(result);
 }
-export const getLector = (req,res)=>{
-    res.send('Obteniendo lector');
+export const getLector = async (req,res)=>{
+    console.log('Obteniendo lector');
+    console.log(req.params.id);
+    const [result] = await pool.query('SELECT * FROM lectores WHERE id = ?',[req.params.id]);
+    res.json(result[0]);
 }
 export const createLector = async (req,res)=>{
     const {name,last_name,photo,type} = req.body;
